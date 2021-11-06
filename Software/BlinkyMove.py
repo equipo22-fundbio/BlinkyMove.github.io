0673a37c2aa2f64f059c3fb46f8a43f1bc92c541
equipo22-fundbio <89932158+equipo22-fundbio@users.noreply.github.com>
2021-11-05 22:17:08 -0500

    Add files via upload

---
+++ Software/BlinkyMove.py
@@ -0,0 +1,42 @@
+from kivy.app import App
+from kivy.uix.widget import Widget
+from kivy.lang import Builder
+from kivy.uix.screenmanager import ScreenManager, Screen
+
+
+
+
+#Define our different screens
+class FirstWindow(Screen):
+	pass
+
+class SecondWindow(Screen):
+	pass
+
+class ThirdWindow(Screen):
+	pass
+
+class FourthWindow(Screen):
+	pass
+
+class FifthWindow(Screen):
+	pass
+
+class SeventhWindow(Screen):
+	pass
+
+class WindowManager(ScreenManager):
+	pass
+
+# Designate Our .kv design file
+kv = Builder.load_file('main.kv')
+
+
+class BlinkyMove(App):
+	def build(self):
+		return kv
+
+if __name__ == '__main__':
+	BlinkyMove().run()
+
+
